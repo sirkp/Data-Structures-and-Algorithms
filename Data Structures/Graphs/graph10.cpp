feea2bb5304579ea8ba2521b81a5c5d90207914c
Pradeep Kumar <sirkp@gmail.com>
2020-01-24 01:09:55 +0530

    Added Check whether a given graph is Bipartite or not

---
+++ Data Structures/Graphs/graph10.cpp
@@ -0,0 +1,106 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Graph
+{
+private:
+	int V;
+	list<int> *adj;
+public:
+	Graph(int V)
+	{
+		this->V = V;
+		adj = new list<int>[V];
+	}
+	void addEdgeUndirected(int u,int v)
+	{
+		adj[u].push_back(v);
+		adj[v].push_back(u);
+	}
+	void addEdgeDirected(int u, int v){adj[u].push_back(v);}
+
+	bool isBipartite();
+	void print()
+	{
+		for(int i=0;i<V;i++)
+		{
+			cout<<i<<"-->";
+			for(auto x:adj[i])
+			cout<<x<<"->";
+			cout<<endl;	
+		}
+	}
+};
+
+void print_array(int arr[],int n)
+{	
+	cout<<"array looklike\n";
+	for(int i=0;i<n;i++)
+	cout<<arr[i]<<" ";
+	cout<<endl;	
+}
+
+bool Graph::isBipartite()
+{
+	int *color = new int[V];
+	for(int i=0;i<V;i++)
+	color[i] = -1;
+	
+	list<int> queue;
+
+	for(int i=0;i<V;i++)
+	{
+		if(color[i]==-1)
+		{
+			color[i] = 1;	
+			queue.push_back(i);
+
+			while(!queue.empty())
+			{
+				int s = queue.front();
+				queue.pop_front();
+
+				for(auto x:adj[s])
+				{
+					if(x == s)
+					{
+						print_array(color,V);
+						return false;	
+					}
+
+					if(color[x]==-1)
+					{
+						color[x] = 1 - color[s];	
+						queue.push_back(x);
+					}
+					else if(color[x]==color[s])
+					{
+						print_array(color,V);
+						return false;	
+					}	
+				}
+			}
+		}
+	}	
+	print_array(color,V);
+	return true;
+}
+
+int main()
+{
+	#ifndef ONLINE_JUDGE
+		freopen("input.txt","r",stdin);
+		freopen("output.txt","w",stdout);
+	#endif
+	Graph g(5);
+	g.addEdgeUndirected(0,1);
+	g.addEdgeUndirected(1,2);
+	g.addEdgeUndirected(2,3);
+	g.addEdgeUndirected(3,4);
+	g.addEdgeUndirected(4,0);
+	g.print();
+	if(g.isBipartite())
+	cout<<"Bipartite hai be\n";
+	else	
+	cout<<"Bipartite nahi hai!\n";
+}
