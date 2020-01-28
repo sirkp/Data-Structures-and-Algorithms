0119ffc51fc046f18fbc63221f41f4155b56c220
Pradeep Kumar <sirkp@gmail.com>
2020-01-28 23:35:32 +0530

    solved minmise cashflow problem

---
+++ Data Structures/Graphs/graph12.cpp
@@ -0,0 +1,69 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void print(vector<int> amount)
+{
+	for(auto x:amount)
+	cout<<x<<" ";
+	cout<<endl;	
+}
+
+void minCashFlow(int *graph,int N)
+{
+	vector<int> amount(N);
+	for(int i =0;i<N;i++)
+	amount[i] = 0;
+		
+	for(int i =0;i<N;i++)
+	for(int j=0;j<N;j++)
+	amount[j] += *(graph+(N*i)+j) - *(graph+(N*j)+i);
+
+	for(auto itr = amount.begin();itr!=amount.end();itr++)
+	{
+		if(*itr==0)
+		amount.erase(itr);	
+	}
+
+	int count = 0;
+	while((int)amount.size()>0)
+	{
+		auto itrMax = max_element(amount.begin(),amount.end());
+		auto itrMin = min_element(amount.begin(),amount.end());
+		if(abs(*(itrMin))> *(itrMax))
+		{
+			*(itrMin) += *(itrMax);
+			amount.erase(itrMax); 
+		}
+		else if(abs(*(itrMin))< *(itrMax))
+		{
+			*(itrMax) += *(itrMin);
+			amount.erase(itrMin);
+		}
+		else
+		{
+			int temp = (*itrMax);
+			amount.erase(itrMin);
+			amount.erase(find(amount.begin(),amount.end(),temp));
+		}
+		count++;
+	}
+	cout<<count<<endl;
+		
+}
+int main()
+{
+	#ifndef ONLINE_JUDGE
+		freopen("input.txt","r",stdin);
+		freopen("output.txt","w",stdout);
+	#endif
+	int N = 4;
+	int graph[N][N] = {
+						{0,20,0,20},
+						{0,0,25,0},
+						{30,0,0,0},
+						{0,15,10,0},
+						};
+
+	minCashFlow(&graph[0][0],N);					
+
+}
--- Data Structures/Graphs/t.cpp
+++ Data Structures/Graphs/t.cpp
@@ -1,50 +1,19 @@
-//Disjoint Set (Or Union-Find) | Set 1 (Detect Cycle in an Undirected Graph)  https://www.geeksforgeeks.org/union-find/
-// In this post, we will discuss the application of Disjoint Set Data Structure. The application is to check whether a given graph contains a cycle or not.
-// Union-Find Algorithm can be used to check whether an undirected graph contains cycle or not
 #include <bits/stdc++.h>
 using namespace std;
-
-class Edge
-{
-private:
-	int src,dest;
-public:
-	Edge(int src,int dest)
-	{
-		this->src = src;
-		this->dest = dest;
-	}
-	int getSrc(){return src;}
-	int getDest(){return dest;}
-};
-
-class Graph
-{
-private:
-	int V,E;
-	vector<Edge> edge;
-public:
-	
-	Graph(int V,int E)
-	{
-		this->V = V;
-		this->E = E;
-	}
-	void addEdgeUndirected(int u, int v);
-};
-
-void Graph::addEdgeUndirected(int u,int v)
-{
-	Edge temp(u,v);
-	edge.push_back(temp);
-}
-
 int main()
 {
-	Graph g(5,5);
-	g.addEdgeUndirected(0,1);
-	g.addEdgeUndirected(0,2);
-	g.addEdgeUndirected(1,3);
-	g.addEdgeUndirected(1,4);
-	g.addEdgeUndirected(3,4);
+	vector<int> v;
+	v.push_back(1);
+	v.push_back(2);
+
+	vector<int>::iterator itr1 = v.begin()+1;
+	v.erase(v.begin());
+	vector<int>::iterator itr2 = v.begin();
+	vector<int>::iterator itr3 = v.begin()+1;
+
+		
+	cout<<(*itr1)<<" "<<(*itr2)<<" "<<v.size()<<endl;
+
+	if(itr1==itr2)
+	cout<<"Ha\n";	
 }
